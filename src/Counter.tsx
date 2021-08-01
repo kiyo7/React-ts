22e9909ca7ba0201d05e5c2ddffe1f2c7b1506e9
kiyo7 <kiyo324783@icloud.com>
2021-08-01 09:09:04 +0900

    useRef basic

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -18,12 +18,21 @@ const Counter: React.FC<{}> = () => {
     renderTimes.current = renderTimes.current + 1;
   });
 
+  //null!でnullは絶対無いよって感じ 関数の実行時にnullはありえない場合に使う
+  const ref = useRef<HTMLInputElement>(null!);
+
+  const focusInput = () => {
+    ref.current.focus();
+  };
+
   return (
     <>
       <div>value: {value}</div>
       <button onClick={increment}>+1</button>
       <button onClick={decrement}>-1</button>
       <div>This component was re-rendered {renderTimes.current} times.</div>
+      <input ref={ref} type="text" />
+      <button onClick={focusInput}>Click Me!</button>
     </>
   );
 };
